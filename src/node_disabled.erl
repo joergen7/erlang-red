c64cc9d7a75c78a26fdf1e167ebd2994599a7138
Gerrit Riessen <gorenje@posteo.de>
2025-04-08 20:46:27 +0200

    disabled node is now disabled and that's ok - i.e. its not an error if it receives an message

--- src/node_disabled.erl
+++ src/node_disabled.erl
@@ -3,21 +3,21 @@
 %%
 %% No Operation node that is used for all unknown types. It represents
 %% a deadend for a message, it stops here.
+%%
 
+%%
+%% if an disabled node receives a message its not an error, the design is
+%% such that nodes do not know anything about other nodes so if a disabled
+%% node is replaced by this, then so be it. Ignore the message and move on.
+%%
 -export([node_disabled/1]).
 -export([handle_incoming/2]).
 -export([handle_outgoing/2]).
 
-handle_incoming(NodeDef,_Msg) ->
-    {ok, IdStr} = maps:find(id,NodeDef),
-    {ok, TypeStr} = maps:find(type,NodeDef),
-    io:format("Node is disabled (incoming) for [~p](~p)\n",[TypeStr,IdStr]),
+handle_incoming(_NodeDef,_Msg) ->
     ok.
 
-handle_outgoing(NodeDef,_Msg) ->
-    {ok, IdStr} = maps:find(id,NodeDef),
-    {ok, TypeStr} = maps:find(type,NodeDef),
-    io:format("Node is disabled (outgoing) for [~p](~p)\n",[TypeStr,IdStr]),
+handle_outgoing(_NodeDef,_Msg) ->
     ok.
 
 node_disabled(NodeDef) ->
