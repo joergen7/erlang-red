42e0142f84983eb762b7bef62c83f81329436432
Gerrit Riessen <gorenje@posteo.de>
2025-04-15 08:36:02 +0200

    support posting to /settings/user but ignore the data

--- src/http/ered_http_empty_json.erl
+++ src/http/ered_http_empty_json.erl
@@ -1,9 +1,9 @@
 -module(ered_http_empty_json).
 
 %%
-%% Get blower - this returns an empty hash. This is used for returning the
-%% credentials for a flow tab. Double click on a flow tab and this request
-%% is set.
+%% This returns an empty hash. This is used for returning the
+%% credentials for a flow tab. Double click on a flow tab and
+%% this request is set.
 %%
 %% Also used for the contexts - that are forever empty.
 %%
@@ -13,8 +13,10 @@
 -export([
     init/2,
     allowed_methods/2,
-    handle_response/2,
+    handle_get_response/2,
+    handle_post_response/2,
     content_types_provided/2,
+    content_types_accepted/2,
     format_error/2
 ]).
 
@@ -22,18 +24,29 @@ init(Req, State) ->
     {cowboy_rest, Req, State}.
 
 allowed_methods(Req, State) ->
-    {[<<"GET">>], Req, State}.
+    {[<<"GET">>, <<"POST">>], Req, State}.
 
 content_types_provided(Req, State) ->
-    {[{{<<"application">>, <<"json">>, '*'}, handle_response}], Req, State}.
+    {[{{<<"application">>, <<"json">>, '*'}, handle_get_response}], Req, State}.
 
-handle_response(Req, State) ->
+content_types_accepted(Req, State) ->
+    {
+        [{{<<"application">>, <<"json">>, '*'}, handle_post_response}],
+        Req,
+        State
+    }.
+
+handle_get_response(Req, State) ->
     case maps:find(path, Req) of
         {ok, <<"/library/local/flows/">>} ->
             {<<"[]">>, Req, State};
         _ ->
             {<<"{}">>, Req, State}
     end.
+
+handle_post_response(Req, State) ->
+    Resp = cowboy_req:set_resp_body(<<"{\"rev\":\"feed\"}">>, Req),
+    {true, Resp, State}.
 
 format_error(Reason, Req) ->
     {
