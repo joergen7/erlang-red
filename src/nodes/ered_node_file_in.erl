4e14df81e725a8956835b30aaeeb62a353079a61
Gerrit Riessen <gorenje@posteo.de>
2025-04-15 13:46:48 +0200

    file in node can now take a file name from a message property and it generates a debug message if the filename isn't found

--- src/nodes/ered_node_file_in.erl
+++ src/nodes/ered_node_file_in.erl
@@ -9,6 +9,45 @@
 
 -import(node_receivership, [enter_receivership/3]).
 
+
+send_out_debug_msg(NodeDef, Msg, ErrMsg, DebugType) ->
+    IdStr    = ered_nodes:get_prop_value_from_map(id,    NodeDef),
+    ZStr     = ered_nodes:get_prop_value_from_map(z,     NodeDef),
+    NameStr  = ered_nodes:get_prop_value_from_map(name,  NodeDef, <<"file in">>),
+
+    Data = #{
+      id       => IdStr,
+      z        => ZStr,
+      path     => ZStr,
+      name     => NameStr,
+      msg      => ErrMsg,
+      format   => <<"string">>
+     },
+
+    nodered:debug(nodered:ws(Msg), Data, DebugType).
+
+
+
+%% erlfmt:ignore lined up
+debug_msg(NodeDef, Msg, {filename_type_not_supported, FileNameType}) ->
+    ErrMsg = ered_nodes:jstr(
+                 "File name type not supported: ~p",
+                 [FileNameType]
+                ),
+    send_out_debug_msg(NodeDef, Msg, ErrMsg, error);
+
+debug_msg(NodeDef, Msg, {file_not_found, FileName}) ->
+    ErrMsg = ered_nodes:jstr("File Not Found: ~p", [FileName]),
+    send_out_debug_msg(NodeDef, Msg, ErrMsg, error);
+
+debug_msg(NodeDef, _Msg, Opts) ->
+    ered_nodes:this_should_not_happen(
+      NodeDef,
+      io_lib:format("Internal call went wrong ~p",
+                    [Opts])
+    ).
+
+
 %% Attributes of interest:
 %%   "filename": "priv/testdata/helloworld.txt",
 %%   "filenameType": "str",
@@ -18,24 +57,41 @@
 %%   "encoding": "none",
 %%   "allProps": false,
 %%
+get_filename(<<"str">>,NodeDef,_Msg) ->
+    maps:find(filename,NodeDef);
+get_filename(<<"msg">>,NodeDef,Msg) ->
+    {ok,PropName} = maps:find(filename, NodeDef),
+    maps:find(binary_to_atom(PropName),Msg);
+get_filename(FileNameType,NodeDef,Msg) ->
+    debug_msg(NodeDef, Msg, {filename_type_not_supported, FileNameType}),
+    failed.
+
 handle_incoming(NodeDef, Msg) ->
-    {ok, FileName} = maps:find(filename, NodeDef),
-    case file:read_file(FileName) of
-        {ok, FileData} ->
-            Msg2 = maps:put(payload, FileData, Msg),
-            ered_nodes:send_msg_to_connected_nodes(NodeDef, Msg2);
+    {ok, FileNameType} = maps:find(filenameType, NodeDef),
+
+    case get_filename(FileNameType,NodeDef,Msg) of
+        {ok,FileName} ->
+            case file:read_file(FileName) of
+                {ok, FileData} ->
+                    Msg2 = maps:put(payload, FileData, Msg),
+                    ered_nodes:send_msg_to_connected_nodes(NodeDef, Msg2);
+                _ ->
+                    %%
+                    %% TODO this should trigger an exception handler if one
+                    %% TODO is defined within this flow (i.e. the 'z' value
+                    %% TODO defines the flow, is there an catch node for
+                    %% TODO that 'z' value).
+                    debug_msg(NodeDef, Msg, {file_not_found, FileName})
+            end;
+        failed ->
+            ignore;
+
         _ ->
-            %%
-            %% TODO this should trigger an exception handler if one is defined
-            %% TODO within this flow (i.e. the 'z' value defines the flow, is
-            %% TODO there an catch node for that 'z' value).
             ered_nodes:this_should_not_happen(
-                NodeDef,
-                io_lib:format(
-                    "File In Node: File not found [~p]\n",
-                    [FileName]
-                )
-            )
+              NodeDef,
+              io_lib:format("file in error in obtaining filename [~p] (~p)\n",
+                            [NodeDef,Msg])
+             )
     end,
 
     NodeDef.
