42e7471413a8d456db8bd216846793610fcb0cb3
Gerrit Riessen <gerrit@flowhub.org>
2025-06-08 20:25:24 +0200

    Function node timeout implementation.
    This implements the timeout feature of a function node and it has many
    edgecases. What happens if timeout expires and the node is being supervised
    by a supervisor mode? What happens with subprocesses when a function node
    gets killed by a supervisor node? What happens when the func code is broken?
    
    Basically the function ndoe is a bit jekyll & hyde: it behaviours slightly
    differently when supervised than when it's not being supervised. The reason
    for this is that processes for nodes should not go down unless they are
    managed by supervisor nodes. But a function node creates two subprocesses per
    message and normally if these subprocesses fail, the node process does not
    care - it remains up. Processes for nodes don't die.
    
    But that's not the case when the node is being supervised. That is because
    the supervisor node supervises the process for the node, not the subprocesses
    that the function node might or might not create. So to indicate to the
    supervisor node that something went wrong, the node process has to die.
    
    It gets even worse if the supervisor has a restart policy of "one for all"
    or "rest for one" - the supervisors killing innocent function nodes just
    because a sibling node was malfunctioning. So how to stop the healthy
    subprocesses of a healthy function node if an unhealthy third party node fails
    and the supervisor needs to cleanup?
    
    Solution is quite simple: each subprocess of a function node monitors the
    node process. Node process goes down, the subprocess gets a message and also
    dies. Of course it goes in both direction: subprocess unexpectedly dies, the
    node process also gets informed and decides what to do.
    
    All this is tested by a bunch of "unit flow tests" for the function and
    supervisor nodes:
    
    - https://flows.red-erik.org/f/f2f61ec9fc46a468
    - https://flows.red-erik.org/f/49c0a3ed69d61f56
    - https://flows.red-erik.org/f/442b3bf0d630e21d
    - https://flows.red-erik.org/f/d97b4eeb4ce93f0b
    - https://flows.red-erik.org/f/c5f513bc380bc30a

---
+++ src/managers/ered_function_code_manager.erl
@@ -0,0 +1,265 @@
+-module(ered_function_code_manager).
+
+-behaviour(gen_server).
+
+-export([
+    init/1,
+    code_change/3,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    stop/0,
+    start/2
+]).
+
+-export([
+    execute_sync/3,
+    perform_func_code/3
+]).
+
+%%
+%% This manager is spun up once per message to handle the execution of the
+%% Erlang code contained in the function node. The execution needs to be managed
+%% because the code could exit unexpectedly by having an "exit(...)" statement
+%% or could timeout because it took too long or it could have an badarg exception
+%% because it failed something else. These conditions need to be caught here
+%% and communicated back to the original node process that start this manager.
+%%
+-import(ered_nodes, [
+    jstr/1,
+    jstr/2,
+    post_exception_or_debug/3,
+    send_msg_on/2
+]).
+
+-import(ered_nodered_comm, [
+    node_status/5,
+    ws_from/1
+]).
+
+-define(CODE_WRAPPER, fun() ->
+    Msg2 = Func(NodeDef, Msg),
+    Receiver ! {function_eval_result, Msg2, Msg}
+end).
+
+-define(POST_OFF_ERROR,
+    ErrMsg = jstr("Stanza: {{{ ~p }}} Error: ~p", [jstr(ErlangCode), Error]),
+    node_status(ws_from(Msg), NodeDef, "parse error", "red", "dot"),
+    post_exception_or_debug(NodeDef, Msg, ErrMsg)
+).
+
+-define(POST_TO_PARENT(Reason),
+    is_process_alive(From) andalso
+        (From ! {'DOWN', func_code_mgr, msg, Msg, Reason})
+).
+
+%%
+%%
+start(NodeDef, Msg) ->
+    gen_server:start(?MODULE, {NodeDef, Msg, self(), undefined}, []).
+
+init(Args) ->
+    {ok, Args}.
+
+%%
+%%
+%%  ------------------ call
+
+handle_call(Msg, _From, State) ->
+    io:format("FUNC Manager Call: ~p~n", [Msg]),
+    {reply, ok, State}.
+
+%%
+%%
+%%  ------------------ cast
+
+handle_cast(Msg, State) ->
+    io:format("FUNC Manager Cast: ~p~n", [Msg]),
+    {noreply, State}.
+
+%%
+%%  ------------------ info
+
+% This kicks off a separate process for executing the actual code. It stores
+% the process id into the state so that the timeout trigger can kill it
+% if needed.
+handle_info(perform_func_code, {NodeDef, Msg, From, undefined}) ->
+    % if the subprocess code contains an "exit(self(),...)" call then we
+    % want to know about it. So we can pass up the exit reason to the node
+    % and the node process can decided whether to go down (if supervised) or
+    % ignore the exit.
+    process_flag(trap_exit, true),
+
+    % we monitor our parent, if it dies, then kill everything here.
+    % this happens when a supervisor kills the node because another node
+    % went down and the restart policy is "one for all" or "rest for one".
+    erlang:monitor(process, From),
+
+    case maps:get(timeout, NodeDef) of
+        infinity ->
+            ignore;
+        Timeout ->
+            erlang:start_timer(Timeout, self(), kill_process)
+    end,
+
+    ExecPid = erlang:spawn_link(
+        ?MODULE,
+        perform_func_code,
+        [NodeDef, Msg, From]
+    ),
+    {noreply, {NodeDef, Msg, From, ExecPid}};
+% timeout was triggered but has the execute process already completed?
+% if so, then we can silently stop. Else we raise hell: kill the execute
+% process, inform the parent and post exception.
+handle_info(
+    {timeout, _TRef, kill_process},
+    {_NodeDef, Msg, From, ExecPid} = State
+) ->
+    case is_process_alive(ExecPid) of
+        true ->
+            exit(ExecPid, kill),
+            ?POST_TO_PARENT(timeout_killing);
+        false ->
+            ignore
+    end,
+    {stop, normal, State};
+% our parent was killed, we kill the exec pid and stop ourselves.
+handle_info(
+    {'DOWN', _, process, From, killed},
+    {_NodeDef, _Msg, From, ExecPid} = State
+) ->
+    exit(ExecPid, kill),
+    {stop, normal, State};
+% so the execute pid exited because there was an exit(self(),...) call
+% in the code. We pass this up to our parent and let them deal with it.
+handle_info(
+    {'EXIT', ExecPid, Reason},
+    {_NodeDef, Msg, From, ExecPid} = State
+) ->
+    ?POST_TO_PARENT(Reason),
+    {stop, normal, State};
+% catch all.
+handle_info(_, State) ->
+    {noreply, State}.
+
+%%
+%%
+code_change(_OldVersion, State, _Extra) ->
+    {ok, State}.
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
+
+%%
+%%
+terminate(normal, _State) ->
+    ok;
+terminate(Event, _State) ->
+    io:format("FUNC manager terminated with {{{ ~p }}}~n", [Event]),
+    ok.
+
+%%
+%% ------------------------ Helpers
+%%
+
+%%
+%%
+perform_func_code(NodeDef, Msg, From) ->
+    case maps:find(func, NodeDef) of
+        {ok, <<>>} ->
+            post_exception_or_debug(
+                NodeDef,
+                Msg,
+                <<"empty function code, doing nothing">>
+            );
+        {ok, Code} ->
+            NewMsg = execute_sync(
+                io_lib:format("fun(NodeDef,Msg) -> ~n ~s ~n end.", [Code]),
+                NodeDef,
+                Msg
+            ),
+
+            case send_message_on_ports(maps:get(wires, NodeDef), NewMsg) of
+                unacceptable_response ->
+                    Msg2 = Msg#{failed_content => NewMsg},
+                    post_exception_or_debug(
+                        NodeDef,
+                        Msg2,
+                        <<"unacceptable response">>
+                    );
+                _ ->
+                    is_process_alive(From) andalso
+                        gen_server:cast(From, {func_completed_with, Msg})
+            end;
+        _ ->
+            post_exception_or_debug(
+                NodeDef,
+                Msg,
+                <<"function code not found">>
+            )
+    end.
+
+%%
+%%
+execute_sync(ErlangCode, NodeDef, Msg) ->
+    % this execute is performed by start and finalize code and returns
+    % a NodeDef map.
+    case evaluate_erlang(ErlangCode) of
+        {ok, Func} ->
+            Func(NodeDef, Msg);
+        Error ->
+            ?POST_OFF_ERROR,
+            NodeDef
+    end.
+
+%%
+%%
+handle_local_function(FunctionName, Args) ->
+    io:format("~p(~p)", [FunctionName, Args]).
+
+%%
+%% This does no run the code, this compiles a function that contains the
+%% the code. The funcion is then executed in the execute/4 function.
+evaluate_erlang(Expression) when is_list(Expression) ->
+    evaluate_erlang(list_to_binary(Expression));
+evaluate_erlang(Expression) ->
+    case erl_scan:string(binary_to_list(Expression)) of
+        {ok, Tokens, _} ->
+            case erl_parse:parse_exprs(Tokens) of
+                {ok, Parsed} ->
+                    case
+                        erl_eval:exprs(
+                            Parsed,
+                            [],
+                            {value, fun handle_local_function/2}
+                        )
+                    of
+                        {value, Result, _} ->
+                            {ok, Result}
+                    end;
+                Error ->
+                    {error, Error}
+            end;
+        Error ->
+            {error, Error}
+    end.
+
+%%
+%% Support multiple output ports. For that the message returned has to be
+%% a list containing message objects, i.e., maps.
+send_message_on_ports(_, undefined) ->
+    done;
+send_message_on_ports(Wires, NewMsg) when is_map(NewMsg) ->
+    send_message_on_ports(Wires, [NewMsg]);
+send_message_on_ports([], _) ->
+    done;
+send_message_on_ports(_, []) ->
+    done;
+send_message_on_ports([_ | MoreWires], [undefined | MoreMsgs]) ->
+    send_message_on_ports(MoreWires, MoreMsgs);
+send_message_on_ports([Wires | MoreWires], [Msg | MoreMsgs]) when is_map(Msg) ->
+    send_msg_on(Wires, Msg),
+    send_message_on_ports(MoreWires, MoreMsgs);
+send_message_on_ports(_, _) ->
+    unacceptable_response.
--- src/nodes/ered_node_function.erl
+++ src/nodes/ered_node_function.erl
@@ -30,111 +30,206 @@
 %%             ]
 %%         ]
 
--import(ered_nodes, [
-    jstr/2,
-    post_exception_or_debug/3,
-    send_msg_to_connected_nodes/2
+%%
+%% The function node was fine until I decided to implement the timeout feature.
+%% This was done fairly simply by spawning out a new process for the execution
+%% of the code and additionaly a second process is spun up that is a timer
+%% to kill the process if it takes longer than the timeout to run.
+%%
+%% This worked wonderfully especially since it is necessary to spin up two
+%% processes for *each* message received. If the timer is triggered, this
+%% node (there being exactly one process for this node) receives the timeout
+%% message with a process id. The process id is of the process executing the
+%% function code.
+%%
+%% This node checks whether the function-code-execution process is still
+%% alive (it might have already exited normally). If the process is alive,
+%% then this node will kill the process *and* post off an exception indicating
+%% that the timeout was triggered. If the process *isn't* alive, then this node
+%% does exactly nothing, nada, nichts.
+%%
+%% Great that works rather well. And is - IMHO - an elegant solution to the
+%% problem of implementing the timeout for a function.
+%%
+%% However what happens when a supervisor comes along and is supervising this
+%% node. Again, we have the same issue as with the supervisor-of-supervisor
+%% problem: when does a supervisor node fail? One important rule I have is that
+%% node processes don't go down. They have to maintain the flow editor frontend
+%% with their status. Processes representing nodes DON'T die .... unless they
+%% are supervised by the loving grace of a supervisor. Supervisor nodes then
+%% are responsible for restarting nodes. But nodes that have sub-processes
+%% that they - in turn - manage are a problem.
+%%
+%% What happens when the subprocess dies? In a normal case what happens is that
+%% the node doesn't care. Let the subprocess die. The node will restart it
+%% in due course. This changes when the subprocess represents the functionality
+%% of the node, i.e., if the subprocess dies, then there is an inconsistency
+%% in the functionality if the node so it should indicate that by also dying.
+%% But only if its being supervised - so that it can either be restarted or
+%% stop working - since "stop working" is part of the functionality of the
+%% node, not the process.
+%%
+%% Now, here is the problem: if the function node is killed by a supervisor
+%% because another node went down *and* the supervisor has restart policy
+%% of "one for all" or "rest for one", then this node has to be killed even
+%% though it might be functioning perfectly well. When this happens, this node
+%% has to kill all its subprocesses executing function code (the timeout
+%% processes will take care of itself) and then the node has to exit itself.
+%%
+%% Good sounds straightforward. Problem is that this node has no list of
+%% subprocesses and nor does it want to. Since a new process is spun up for
+%% each message this node receives, that could become a long list.
+%%
+%% Using an erlang:link has the problem that its bidirectional. So that if the
+%% subprocess goes down, it takes this node with it. I only want the subprocess
+%% to go down when this node goes down.
+%%
+%% The current solution is to move most of the logic to the
+%% ered_function_code_manager module and have it deal with it - done. Oh, you
+%% say and who is going to code that code?!?! Doh. Life simply cannot be easy.
+%%
+%% Anyway it was easy. The ered_function_code_manager is monitoring this
+%% process and kills its subprocess if this node goes down. The manager only
+%% deals with one message, i.e., a manager process is spun up by this node
+%% for each message it receives. There are bunch of edge cases, all documented
+%% with test flows. This node has slightly different behaviour if supervised
+%% by a supervisor node. All sorts of goodies.
+%%
+
+-import(ered_function_code_manager, [
+    execute_sync/3
 ]).
+
+-import(ered_nodes, [
+    jstr/1,
+    jstr/2,
+    post_exception_or_debug/3
+]).
+
+-define(EXECUTE_CODE_SYNC,
+    execute_sync(
+        io_lib:format("fun(NodeDef,Msg) -> ~n ~s ~n end.", [Code]),
+        NodeDef,
+        #{'_ws' => WsName}
+    )
+).
+
+%% The node process should never go down. That is the premise upon which we
+%% build the software. However, this is not true if the node is being supervised.
+%% In that case, if *any* subprocess goes down, then the node process goes
+%% down to indicate to the supervisor that failure has happened.
+-define(EXIT_WHEN_SUPERVISED(Why),
+    case maps:find('_fail_on_supervisor_death', NodeDef) of
+        {ok, true} ->
+            exit(self(), Why);
+        _ ->
+            post_exception_or_debug(
+                NodeDef,
+                maps:put(cause, Why, Msg),
+                <<"unexpected exit">>
+            )
+    end
+).
 
 %%
 %%
 start(NodeDef, _WsName) ->
-    ered_node:start(NodeDef, ?MODULE).
+    % Ensure timeout value is set to a number and converted to milliseconds
+    % or infinity if the value is negative or zero.
+    ered_node:start(
+        case maps:find(timeout, NodeDef) of
+            {ok, Val} ->
+                try
+                    case binary_to_integer(Val) * 1000 of
+                        N when N < 0 ->
+                            NodeDef#{timeout => infinity};
+                        N when N =:= 0 ->
+                            NodeDef#{timeout => infinity};
+                        N ->
+                            NodeDef#{timeout => N}
+                    end
+                catch
+                    _E:_F:_S ->
+                        NodeDef#{timeout => infinity}
+                end;
+            _ ->
+                NodeDef#{timeout => infinity}
+        end,
+        ?MODULE
+    ).
 
+%%
+%% Execute the "On Start" code once this node has been regisgtered
 handle_event({registered, WsName, _Pid}, NodeDef) ->
     case maps:find(initialize, NodeDef) of
         {ok, <<>>} ->
             NodeDef;
         {ok, Code} ->
-            execute(
-                io_lib:format("fun(NodeDef,Msg) -> ~n ~s ~n end.", [Code]),
-                NodeDef,
-                #{'_ws' => WsName}
-            );
+            ?EXECUTE_CODE_SYNC;
         _ ->
             NodeDef
     end;
+%%
+%% Execute the "On Stop" code when the node is stopped. Which is the same
+%% as when this node is killed....
+%% TODO: ensure that folks don't put anything important in the "On Stop" tab
+%% TODO: because the code gets rarely executed (it would have to be executed
+%% TODO: each time this node is killed by a supervisor - for example - I don't
+%% TODO: think that happens at the moment).
 handle_event({stop, WsName}, NodeDef) ->
     case maps:find(finalize, NodeDef) of
         {ok, <<>>} ->
             NodeDef;
         {ok, Code} ->
-            execute(
-                io_lib:format("fun(NodeDef,Msg) -> ~n ~s ~n end.", [Code]),
-                NodeDef,
-                #{'_ws' => WsName}
-            );
+            ?EXECUTE_CODE_SYNC;
         _ ->
             NodeDef
     end;
-handle_event(_, NodeDef) ->
+%%
+%% The down event is generated when the spawned process that is executing the
+%% code goes down.
+handle_event({'DOWN', func_code_mgr, msg, _Msg, normal}, NodeDef) ->
+    % ignore only "normal" exits because I read this:
+    % https://stackoverflow.com/questions/42123376/how-stop-terminate-shutdown-and-exit-are-related/42124862#42124862
+    %
+    % """
+    %   Ordinary messages are really a special kind of signal that always go
+    %   to the mailbox; exit signals (except for normal) cause receivers to also
+    %   die unless they trap exits, so that groups of linked processes can be
+    %   brought down together.
+    % """
+    %
+    % So this means that the function process also goes down when it receives
+    % a non-normal exit, any for that matter.
+    NodeDef;
+handle_event({'DOWN', func_code_mgr, msg, Msg, timeout_killing}, NodeDef) ->
+    % The process running our code was killed by the timeout monitoring
+    % what does this mean for our supervisor? Is this is a fatal exit for
+    % us or do we handle it? Answer: if something takes too long, then it's
+    % a reason to exit but only if we're being supervised by the loving grace
+    % of something great than ourselves.
+    ?EXIT_WHEN_SUPERVISED(timeout),
+    NodeDef;
+handle_event({'DOWN', func_code_mgr, msg, Msg, Reason}, NodeDef) ->
+    % exit but only exit if there is a supervisor watching over us, this
+    % implements the idea that nodes don't die - they just grow old.
+    % The node is responsible for updating the frontend, so the process should
+    % only go down if there is a coordinated restart, i.e., a supervisor
+    % watching over us with loving grace.
+    ?EXIT_WHEN_SUPERVISED(Reason),
+    NodeDef;
+handle_event(E, NodeDef) ->
+    io:format("Recev (handle_event): ~p ~n", [E]),
     NodeDef.
 
+%%
+%%
 handle_msg({incoming, Msg}, NodeDef) ->
-    %% TODO don't support multiple outputs
-    %% TODO support modificiations to the NodeDef.
-    Msg2 =
-        case maps:find(func, NodeDef) of
-            {ok, <<>>} ->
-                Msg;
-            {ok, Code} ->
-                execute(
-                    io_lib:format(
-                        "fun(NodeDef,Msg) -> ~n ~s ~n end.",
-                        [Code]
-                    ),
-                    NodeDef,
-                    Msg
-                );
-            _ ->
-                Msg
-        end,
-    send_msg_to_connected_nodes(NodeDef, Msg2),
-    {handled, NodeDef, Msg2};
-handle_msg(_, NodeDef) ->
+    {ok, Pid} = ered_function_code_manager:start(NodeDef, Msg),
+    Pid ! perform_func_code,
+    {handled, NodeDef, dont_send_complete_msg};
+handle_msg({func_completed_with, Msg}, NodeDef) ->
+    {handled, NodeDef, Msg};
+handle_msg(Msg, NodeDef) ->
+    io:format("Unhandled (handle_msg): ~p ~n", [Msg]),
     {unhandled, NodeDef}.
-
-%%
-%%
-execute(ErlangCode, NodeDef, Msg) when is_list(ErlangCode) ->
-    execute(list_to_binary(ErlangCode), NodeDef, Msg);
-execute(ErlangCode, NodeDef, Msg) ->
-    case evaluate_erlang(binary_to_list(ErlangCode)) of
-        {ok, Func} ->
-            Func(NodeDef, Msg);
-        Error ->
-            ErrMsg = io_lib:format(
-                "Stanza: {{{ ~p }}} Error: ~p",
-                [ErlangCode, Error]
-            ),
-
-            post_exception_or_debug(NodeDef, Msg, ErrMsg),
-            NodeDef
-    end.
-
-%%
-%%
-handle_local_function(FunctionName, Args) ->
-    io:format("~p(~p)", [FunctionName, Args]).
-
-evaluate_erlang(Expression) ->
-    case erl_scan:string(Expression) of
-        {ok, Tokens, _} ->
-            case erl_parse:parse_exprs(Tokens) of
-                {ok, Parsed} ->
-                    case
-                        erl_eval:exprs(
-                            Parsed,
-                            [],
-                            {value, fun handle_local_function/2}
-                        )
-                    of
-                        {value, Result, _} ->
-                            {ok, Result}
-                    end;
-                Error ->
-                    {error, Error}
-            end;
-        Error ->
-            {error, Error}
-    end.
