e18aab7555b1831fefb0c0e67f0565ecbd5ad6a1
Gerrit Riessen <gerrit@flowhub.org>
2025-06-18 09:20:34 +0200

    included an include header

---
+++ include/ered_nodes.hrl
@@ -0,0 +1,14 @@
+-define(NODE_ID_AND_TYPE(NodeDef),
+    {maps:get(<<"id">>, NodeDef), maps:get(<<"type">>, NodeDef)}
+).
+
+-define(BASE_DATA, #{
+    <<"id">>   => get_prop_value_from_map(<<"id">>, NodeDef),
+    <<"z">>    => get_prop_value_from_map(<<"z">>, NodeDef),
+    <<"path">> => get_prop_value_from_map(<<"z">>, NodeDef),
+    <<"name">> => get_prop_value_from_map(
+        <<"name">>,
+        NodeDef,
+        get_prop_value_from_map(<<"type">>, NodeDef)
+    )
+}).
